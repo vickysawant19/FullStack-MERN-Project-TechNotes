abd5ae863fd7d8fef54f1de694b19894270c02cb
vickysawant19 <vicky.sawant19@gmail.com>
2024-04-08 12:16:35 +0530

    ready to deploy

--- Backend/config/allowedOrigin.js
+++ Backend/config/allowedOrigin.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
-  "http://localhost:3000",
-  "http://mysite.com",
-  "http://localhost:5173",
-  "http://192.168.1.7:5173",
+  // "http://localhost:3000",
+  "https://dandd-technotes.onrender.com",
+  // "http://localhost:5173",
+  // "http://192.168.1.7:5173",
 ];
 
 module.exports = allowedOrigins;
